0e52b3bca4dc8e3a20a16de0cc37b61f82e46aba
baptklein <57224454+baptklein@users.noreply.github.com>
2021-10-03 17:54:14 +0200

    Add files via upload

--- plots.py
+++ plots.py
@@ -6,13 +6,11 @@ Created on Sep 2021
 
 import numpy as np
 import os
-
 import matplotlib.pyplot as plt
 import matplotlib
 from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
                                AutoMinorLocator)
 import matplotlib.ticker as ticker
-
 import warnings
 import matplotlib.cbook
 warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
@@ -28,8 +26,24 @@ matplotlib.rc('font', **font)
 matplotlib.rc('axes', **axes)
 
 
-
+# -----------------------------------------------------------
+# Plot a number nb of spectra, each spectrum being vertically
+# shifted by eps wrt the previous one.
+# Possibility to overplot a given spectrum (or prediction) over
+# each spectrum.
+# -----------------------------------------------------------
 def plot_spec_fit(W,I,I_pred=[],eps=0.03,nb=10):
+
+    """
+    --> Inputs:     - W: wavelength vector
+                    - I: Matrix of spectra (time,wavelength)
+                    - I_pred: Matrix of spectra to superimpose to I (same dimension as I)
+                    - eps: vertical shift between consecutive spectra
+                    - nb: number of spectra to plot   
+
+    --> Outputs:    - Plot displayed
+
+     """
 
     plt.figure(figsize=(20,14))
     for ii in range(nb):
@@ -42,8 +56,25 @@ def plot_spec_fit(W,I,I_pred=[],eps=0.03,nb=10):
 
 
 
-
+# -----------------------------------------------------------
+# Plot a given number of spectra, each spectrum being vertically
+# shifted wrt the previous one. For each spectrum, we overplot 
+# the spectrum after outlier correction and the normalization
+# solution.
+# -----------------------------------------------------------
 def plot_normalization(Ws,Is,I_corr,I_med_tot,eps=0.04,nb=10):
+
+    """
+    --> Inputs:     - Ws: wavelength vector
+                    - Is: Matrix of spectra (time,wavelength) -- before normalization (end of step 1.3)
+                    - I_corr: Matrix of spectra corrected from outliers but not normalized (same dimensions as Is)
+                    - I_med_tot: Normalization solution
+                    - eps: vertical shift between consecutive spectra
+                    - nb: number of spectra to plot   
+
+    --> Outputs:    - Plot displayed
+
+     """
 
     plt.figure(figsize=(15,20))
     for nn in range(nb):
@@ -59,29 +90,48 @@ def plot_normalization(Ws,Is,I_corr,I_med_tot,eps=0.04,nb=10):
 
 
 
-
+# -----------------------------------------------------------
+# Plot relevant quantities evolving with time:
+# - Transit flux
+# - airmass
+# - Velocimetric correction between Geocentric frame and stellar rest frame
+# - Max S/N
+# -----------------------------------------------------------
 def plot_timeseries(T_obs,T0,Porb,phase,flux,n_ini,n_end,airmass,Vc,snr_mat):
 
-    T_wrt  = 24.*(T_obs-T0 - int((T_obs[-1]-T0)/Porb)*Porb)
+    """
+    --> Inputs:     - T_obs: time vector
+                    - T0: mid-transit time [same unit at T_obs]
+                    - Porb: planet orbital period [same unit as T_obs]
+                    - phase: planet orbital phase
+                    - flux: planet-induced transit flux
+                    - n_ini: index of the last observation time before the transit
+                    - n_end: index of the first observation time after the transit
+                    - airmass: vector of airmass values
+                    - Vc: RV correction --> Geocentric to stellar rest frame
+                    - snr_mat: 2D matrix of S/N values in the (order,time) space
 
-    ### 1 - Time series
-    ypad = 10
+    --> Outputs:    - Plot displayed
+
+     """
+
+    T_wrt  = 24.*(T_obs-T0 - int((T_obs[-1]-T0)/Porb)*Porb) # Time with respect to mid-transit in hour
+    ypad   = 10  # pad of the y label
 
     plt.figure(figsize=(12,10))
-    ax  = plt.subplot(411)
 
+    # Transit flux
+    ax  = plt.subplot(411)
     ax.plot(T_wrt,flux,"-+k")
     ax.axvline(T_wrt[n_ini],ls=":",color="r")
     ax.axvline(T_wrt[n_end],ls=":",color="r")
     ax.set_xticks([])
-
     ax2 = ax.twiny() 
     ax2.plot(phase,flux,"-",lw=0.0)
     ax2.set_xlabel("Orbital phase")
-
     ax.set_ylabel("Transit curve\n", labelpad=ypad)
 
-
+    # Airmass
     ax = plt.subplot(412)
     plt.plot(T_wrt,airmass,"-k")
     plt.axvline(T_wrt[n_ini],ls=":",color="r")
@@ -89,6 +139,7 @@ def plot_timeseries(T_obs,T0,Porb,phase,flux,n_ini,n_end,airmass,Vc,snr_mat):
     plt.xticks([])
     ax.set_ylabel("Airmass\n", labelpad=ypad)
 
+    # RV correction between Geocentric frame and stellar rest frame
     ax = plt.subplot(413)
     plt.plot(T_wrt,Vc,"-k")
     plt.axvline(T_wrt[n_ini],ls=":",color="r")
@@ -96,6 +147,7 @@ def plot_timeseries(T_obs,T0,Porb,phase,flux,n_ini,n_end,airmass,Vc,snr_mat):
     plt.xticks([])
     ax.set_ylabel("RV correction\n[km/s]", labelpad=ypad)
 
+    # Maximum S/N
     ax = plt.subplot(414)
     plt.plot(T_wrt,np.max(snr_mat,axis=1),"+k")
     plt.axvline(T_wrt[n_ini],ls=":",color="r")
@@ -107,9 +159,30 @@ def plot_timeseries(T_obs,T0,Porb,phase,flux,n_ini,n_end,airmass,Vc,snr_mat):
     plt.show()
 
 
+
+# -----------------------------------------------------------
+# Colormap representation of any given matrix
+# -----------------------------------------------------------
 def plot_2D(x,y,Z,lab,cmap="gist_heat",n_ini=-1,n_end=-1,title="",size=(12,7)):
 
+    """
+    --> Inputs:     - x: X vector (1D)
+                    - y: Y vector (1D)
+                    - Z: 2D matrix to plot
+                    - lab: list of labels [X_label,Y_label,Z_label]
+                    - cmap: color map
+                    - n_ini: index of last observation before the transit (only indicated if >-1)
+                    - n_end: index of first observation after the transit (only indicated if >-1)
+                    - title: title of the figure
+                    - size: tuple --> size of the plot
+
+    --> Outputs:    - Plot displayed
+
+     """
+
     X,Y  = np.meshgrid(x,y)
+
+    # minilum and maximum values of the color bar (set at -/+ 3 std)
     zmin = np.median(Z) - 3.*Z.std()
     zmax = np.median(Z) + 3.*Z.std()
 
@@ -128,44 +201,41 @@ def plot_2D(x,y,Z,lab,cmap="gist_heat",n_ini=-1,n_end=-1,title="",size=(12,7)):
     plt.show()
 
 
-def compare_2D(x,y,Z,lab,cmap="gist_heat",n_ini=-1,n_end=-1,title="",size=(12,7)):
-
-
-    X,Y  = np.meshgrid(x,y)
-    zmin = np.median(Z) - 2.*Z.std()
-    zmax = np.median(Z) + 2.*Z.std()
-
-    plt.figure(figsize=size)
-    ax   = plt.subplot(111)
-    c    = plt.pcolor(X,Y,Z,cmap=cmap,vmin=zmin,vmax=zmax)
-    cb   = plt.colorbar(c,ax=ax)
-    ax.set_ylim(np.min(y),np.max(y))
-    ax.set_xlim(np.min(x),np.max(x))
-    if n_ini>=0: plt.axhline(y[n_ini],color="#EFFBFF",ls="--",lw=5.0)
-    if n_end>=0: plt.axhline(y[n_end],color="#EFFBFF",ls="--",lw=5.0)
-    ax.set_xlabel(lab[0])
-    ax.set_ylabel(lab[1])
-    cb.set_label(lab[2],rotation=270,labelpad=40)
-    if len(title)>0: ax.set_title(title)
-    plt.show()
 
 
 
 
-
-
-
+# -----------------------------------------------------------
+# Plot a given function z as a function of the order number
+# (bottom X axis) and mean wavelength (top X-axis)
+# -----------------------------------------------------------
 def plot_orders(z,wmean,orders,ind_rem=[],laby=""):
 
+    """
+    --> Inputs:     - z: function to plot (1D vector)
+                    - wmean: Mean wavelengths of z values
+                    - orders: orders number of z values
+                    - ind_rem: if different than [], then highlight in red points whose indices are in ind_rem
+                    - laby: label of Y-axis
+
+    --> Outputs:    - Plot displayed
+
+     """
+
+    # Get finely-sampled wavelength-order number correspondance 
     WW,LO_pred,LO_predt = src.fit_order_wave(orders,wmean)
 
     plt.figure(figsize=(12,5))
+
+    # Bottom X-axis
     ax = plt.subplot(111)
-    ax.plot(orders,z,"*k")
+    ax.plot(orders,z,"*k") # Order number vs z
     if len(ind_rem) > 0:
         ax.plot(np.array(orders)[ind_rem],z[ind_rem],"^r")
     ax.set_ylabel(laby)
     ax.set_xlabel("Order")
+
+    # Top X-axis
     ax2 = ax.twiny()
     ax2.set_xticks(LO_pred)
     ax2.set_xlabel("Wavelength [nm]")
@@ -178,11 +248,28 @@ def plot_orders(z,wmean,orders,ind_rem=[],laby=""):
 
 
 
-def plot_distrib(W,rms_px,rms_pred,W_fin,rms_fin,numb):
+# -----------------------------------------------------------
+# Plot a distribution as well as the best-fitting solution
+# Possibility to plot distribution evolution after outlier removal
+# -----------------------------------------------------------
+def plot_distrib(W,rms_px,rms_pred,W_fin=[],rms_fin=[],numb=0):
+
+    """
+    --> Inputs:     - W: Wavelength vector
+                    - rms_px: distribution to plot
+                    - rms_pred: best-fitting prediction
+                    - W_fin: if different than [], wavelength vector of data after outlier cleaning
+                    - I_fin: Data after outlier cleaning
+                    - numb: order number (for plot title)
+
+    --> Outputs:    - Plot displayed
+
+     """
 
     plt.figure(figsize=(8,5))
     plt.plot(W,rms_px,"+",color="r",alpha=0.75)
-    plt.plot(W_fin,rms_fin,"+k")
+    if len(W_fin) != 0:
+        plt.plot(W_fin,rms_fin,"+k")
     plt.plot(W,rms_pred,"-b",lw=2.0)
     plt.xlabel("Wavelength [nm]")
     plt.ylabel("Dispersion")
@@ -190,11 +277,24 @@ def plot_distrib(W,rms_px,rms_pred,W_fin,rms_fin,numb):
     plt.title(tt)
     plt.show()
 
+
+
         
 
+# -----------------------------------------------------------
+# Compare the dispersion at the center of each spectrum (in 
+# each order) to the photon noise provided by the SPIRou DRS
+# -----------------------------------------------------------
 def plot_spectrum_dispersion(lord):
 
+    """
+    --> Inputs:     - lord: list of Order objects
 
+    --> Outputs:    - Plot displayed
+
+     """
+
+    # Initialization
     rms_sp     = np.zeros(len(lord))
     rms_sp_s   = np.zeros(len(lord))
     rms_drs    = np.zeros(len(lord))
@@ -213,6 +313,7 @@ def plot_spectrum_dispersion(lord):
         wmean[kk]      = O.W_mean
         LO[kk]         = O.number
 
+    # Compute wavelength-order number correspondance
     WW,LO_pred,LO_predt = src.fit_order_wave(LO,wmean)
     plt.figure(figsize=(12,5))
     ax = plt.subplot(111)
@@ -235,8 +336,16 @@ def plot_spectrum_dispersion(lord):
 
 
 
+
+# -----------------------------------------------------------
+# Plot correlation/significance map in the (Kp,Vsys) space.
+# We give 2 options:
+# - Either simple color map (if sn_cutx = [])
+# - Either color map + 1D cuts at best Kp and Vsys
+# -----------------------------------------------------------
 def plot_correlation_map(Vsys,Kp,sn_map,V_inj=0.0,K_inj=0.0,cmap="gist_heat",sn_cutx=[],sn_cuty=[],levels=10):
 
+    ### Simple color maps
     if len(sn_cutx) == 0:
         plt.figure(figsize=(10,7))
         plt.contourf(Vsys,Kp,sn_map,levels=levels,cmap=cmap)
@@ -248,20 +357,23 @@ def plot_correlation_map(Vsys,Kp,sn_map,V_inj=0.0,K_inj=0.0,cmap="gist_heat",sn_
         cb.set_label(r"Significance [$\sigma$]",rotation=270,labelpad=40)
         plt.show()
 
+    ### Color maps with 1D cuts at best Kp and Vsys
     else:
+        # Create grid of subplots
         fig     = plt.figure(figsize=(12,12))
         grid    = plt.GridSpec(4, 4, hspace=0.1, wspace=0.1)
         main_ax = fig.add_subplot(grid[1:,:3])
         y_hist  = fig.add_subplot(grid[1:,-1], yticklabels=[])
         x_hist  = fig.add_subplot(grid[:1, :3], xticklabels=[])
 
+        # Color map
         main_ax.contourf(Vsys,Kp,sn_map,levels=levels,cmap=cmap)
         main_ax.axhline(K_inj,ls=":",color="w",lw=2.5)
         main_ax.axvline(V_inj,ls=":",color="w",lw=2.5)
         main_ax.set_ylabel(r"K$_{\rm{p}}$ [km/s]")
         main_ax.set_xlabel(r"V$_{\rm{sys}}$ [km/s]")
 
-        # histogram on the attached axes
+        # Cut at best Kp
         x_hist.plot(Vsys,sn_cuty,"-k")
         x_hist.axhline(0.0,ls=":",color="gray")
         x_hist.axvline(V_inj,ls=":",color="r")
@@ -269,6 +381,7 @@ def plot_correlation_map(Vsys,Kp,sn_map,V_inj=0.0,K_inj=0.0,cmap="gist_heat",sn_
         x_hist.set_xlabel(title_x,rotation=0,labelpad=-180)
         x_hist.set_ylabel(r"Significance [$\sigma$]")
 
+        # Cut at best Vsys
         y_hist.plot(sn_cutx,Kp,"-k")
         y_hist.axvline(0.0,ls=":",color="gray")
         y_hist.axhline(K_inj,ls=":",color="r")
